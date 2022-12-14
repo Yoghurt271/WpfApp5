94b7a9b619918ebb0a6a9168e32cde423e2c5c52
Misha Odozhiy <misha.odozhiy.04@mail.ru>
2022-12-14 16:06:32 +0300

    eqw

--- WpfApp5/MainWindow.xaml.cs
+++ WpfApp5/MainWindow.xaml.cs
@@ -27,105 +27,47 @@ namespace WpfApp5
     {
         HtmlWeb web = new HtmlWeb();
         string mxn2;
-        string name;
-        string search = "";
+        string uah2;
         public MainWindow()
         {
             InitializeComponent();
+            ParsMxn();
+            ParsUnh();
         }
 
-        //static string LoadPage(string url)
-        //{
-        //    var result = "";
-        //    var request = (HttpWebRequest)WebRequest.Create(url);
-        //    var response = (HttpWebResponse)request.GetResponse();
 
-        //    if (response.StatusCode == HttpStatusCode.OK)
-        //    {
-        //        var receiveStream = response.GetResponseStream();
-        //        if (receiveStream != null)
-        //        {
-        //            StreamReader readStream;
-        //            if (response.CharacterSet == null)
-        //                readStream = new StreamReader(receiveStream);
-        //            else
-        //                readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
-        //            result = readStream.ReadToEnd();
-        //            readStream.Close();
-        //        }
-        //        response.Close();
-        //    }
-        //    return result;
+        public void ParsMxn()
+        {
+            var url = @"https://ru.investing.com/currencies/rub-mxn";
+            var ExchangeRates = web.Load(url);
 
-        //}
+            string mxn = ExchangeRates.DocumentNode.SelectNodes("/html/body/div[1]/div/div/div/div[2]/main/div/div[1]/div[2]/div[1]/span").First().InnerText;
+            mxn2 = mxn;
+            Yuyu.Items.Add("Пессо");
+        }
 
-        //private void Window_Loaded(object sender, RoutedEventArgs e)
-        //{
-        //    string pageContent = LoadPage(url: @"https://ru.investing.com/currencies/rub-mxn");
-        //    var document = new HtmlDocument();
-        //    document.LoadHtml(pageContent);
+        public void ParsUnh()
+        {
+            var url = @"https://ru.investing.com/currencies/rub-uah";
+            var ExchangeRates = web.Load(url);
 
-        //    HtmlNodeCollection Mxn = document.DocumentNode.SelectNodes("/html/body/div[1]/div/div/div/div[2]/main/div/div[1]/div[2]/div[1]/span");
-        //    mxn2 = Mxn.First().InnerText;
-
-        //    string pageContent2 = LoadPage(url: @"https://ru.investing.com/currencies/rub-uah");
-        //    var document2 = new HtmlDocument();
-        //    document2.LoadHtml(pageContent);
-
-        //    HtmlNodeCollection Uah = document.DocumentNode.SelectNodes("/html/body/div[1]/div/div/div/div[2]/main/div/div[1]/div[2]/div[1]/span");
-        //    uah2 = Uah.First().InnerText;
-
-        //    Valute.Items.Add("Гривны");
-        //    Valute.Items.Add("Песо");
-        //}
-
-        //private void Button_Click(object sender, RoutedEventArgs e)
-        //{
-        //    if (Valute.SelectedIndex == 0)
-        //    {
-        //        Result.Content = "Результат: " + Convert.ToDouble(Count.Text) * Convert.ToDouble(uah2);
-        //    }
-        //    else if (Valute.SelectedIndex == 1)
-        //    {
-        //        Result.Content = "Результат: " + Convert.ToDouble(Count.Text) * Convert.ToDouble(mxn2);
-        //    }
-        //}
-
-
+            string uah = ExchangeRates.DocumentNode.SelectNodes("/html/body/div[1]/div/div/div/div[2]/main/div/div[1]/div[2]/div[1]/span").First().InnerText;
+            uah2 = uah;
+            Yuyu.Items.Add("Гривны");
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Result.Content = Convert.ToDouble(Value.Text) * Convert.ToDouble(mxn2);
-        }
-
-
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            try
+            if (Yuyu.SelectedIndex == 0)
             {
-                search = Poisk.Text;
-                var url = @"https://ru.investing.com/currencies/" + search;
-                var ExchangeRates = web.Load(url);
-
-                string mxn = ExchangeRates.DocumentNode.SelectNodes("/html/body/div[1]/div/div/div/div[2]/main/div/div[1]/div[2]/div[1]/span").First().InnerText;
-                mxn2 = mxn;
-                name = ExchangeRates.DocumentNode.SelectNodes("/html/body/div[1]/div/div/div/div[2]/main/div/div[1]/div[1]/h1").First().InnerText;
-                Valuta.Content = name;
+                Result.Content = "Результат: " + Convert.ToDouble(Value.Text) * Convert.ToDouble(mxn2);
             }
-            catch 
+            else if (Yuyu.SelectedIndex == 1)
             {
-                MessageBox.Show("Введите правильную валюту");
+                Result.Content = "Результат: " + Convert.ToDouble(Value.Text) * Convert.ToDouble(uah2);
             }
-            
+
         }
 
-
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            Window_Loaded(sender, e);
-        }
     }
 }
-
